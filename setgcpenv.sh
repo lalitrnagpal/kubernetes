174208766a636672af9b5cb11b0e2b9c2fdb3f84
Lalit Nagpal <lalit2801@gmail.com>
2020-12-11 20:20:10 +0530

    Environment Setup for GCP

---
+++ setgcpenv.sh
@@ -0,0 +1,75 @@
+
+# Apply Updates
+sudo apt update
+sudo apt upgrade
+
+# Install Java
+sudo apt install -y openjdk-14-jdk
+
+# Install Maven
+sudo apt install -y maven
+mvn -version
+
+# Install GIT
+sudo apt install -y git
+git --version
+
+# Install Docker
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update
+
+apt-cache policy docker-ce
+
+sudo apt install -y docker-ce
+
+# Install Docker Compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Install Node JS
+
+sudo apt install -y nodejs
+
+# Install npm
+
+sudo apt install -y npm
+
+# add these packages 
+
+npm add axios
+
+npm add react-router-dom
+
+npm add formik
+
+# git clone the repository
+
+git clone https://github.com/lalitnagpal/docker.git
+
+# sudo systemctl status docker
+
+# So that we can login to Docker Hub
+
+sudo chmod 666 /var/run/docker.sock
+
+# Restart Docker
+
+sudo systemctl restart docker
+
+# To avoid sudo for running Docker
+
+sudo passwd ${USER}
+
+sudo usermod -aG docker ${USER}
+
+su - ${USER}
+
+id -nG
+
