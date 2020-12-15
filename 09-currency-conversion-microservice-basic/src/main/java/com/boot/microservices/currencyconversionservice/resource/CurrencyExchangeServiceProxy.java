9da0ece88cd037e33522edf71693989ac0bbb4ba
Lalit Nagpal <lalit2801@gmail.com>
2020-12-15 08:51:33 +0530

    Kubernetes Microservice Example

--- 09-currency-conversion-microservice-basic/src/main/java/com/boot/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
+++ 09-currency-conversion-microservice-basic/src/main/java/com/boot/microservices/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://34.69.27.54}:9000")
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:9000")
 public interface CurrencyExchangeServiceProxy {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from,
